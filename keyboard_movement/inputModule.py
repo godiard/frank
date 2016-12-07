1c7206d2ac81a4d64143091445e85a55fab22617
fmieres <magno.f.mieres@gmail.com>
2016-12-07 16:20:24 -0300

    Fixes de constaantes de clase y agregado como parámetro de motor los pines que utiliza

--- keyboard_movement/inputModule.py
+++ keyboard_movement/inputModule.py
@@ -2,7 +2,7 @@ import curses
 from motorModule import Motor
 
 # get the curses screen window
-motor  = Motor()
+motor  = Motor([6, 13, 19, 26])
 screen = curses.initscr()
 
 # turn off input echoing
@@ -22,18 +22,18 @@ try:
             if char == curses.KEY_RIGHT:
               # print doesn't work with curses, use addstr instead
                 print 'right'
-                motor.moveTo(motor.RIGHT)
+                motor.moveTo(Motor.RIGHT)
             elif char == curses.KEY_LEFT:
                 print 'left '
-                motor.moveTo(motor.LEFT)
+                motor.moveTo(Motor.LEFT)
             char = screen.getch()
     
         if char == ord('q'):
             motor.cleanUp()
             break
-        else :
+        else:
             char = screen.getch()
-          
+
 finally:
     # shut down cleanly
     curses.nocbreak(); screen.keypad(0); curses.echo()
--- keyboard_movement/motorModule.py
+++ keyboard_movement/motorModule.py
@@ -2,38 +2,39 @@ import RPi.GPIO as GPIO
 import time
 
 class Motor:
+    RIGHT = 1
+    LEFT  = -1
 
-  def __init__(self):
-      self.delay = 0.005
-      self.pins  = [6, 13, 19, 26]
-      self.move_table = [[1, 1, 0, 0], [0, 1, 1, 0], [0, 0, 1, 1], [1, 0, 0, 1]]
+    def __init__(self, pins):
+        self.delay = 0.005
+        self.pins  = pins
+        self.move_table = [[1, 1, 0, 0], [0, 1, 1, 0], [0, 0, 1, 1], [1, 0, 0, 1]]
           
-      self.RIGHT = 1
-      self.LEFT  = -1
 
-      print "Set mode BCM"
-      GPIO.setmode(GPIO.BCM)
+        print "Set mode BCM"
+        GPIO.setmode(GPIO.BCM)
 
-      for pin in self.pins:
+        for pin in self.pins:
           print "Setup output port", pin
           GPIO.setup(pin, GPIO.OUT)
 
-  # direction can be 1 or -1
-  def moveTo(self, direction):
-      step = 0
-      for i in range(4):
-          for pin in range(4):
-              GPIO.output(self.pins[pin], self.move_table[step][pin])
-            
-          step += direction
+    # direction can be 1 or -1
+    def moveTo(self, direction):
+        step = 0
+        for i in range(4):
+            for pin in range(4):
+                GPIO.output(self.pins[pin], self.move_table[step][pin])
 
-          if direction == self.RIGHT:
-              if step == len(self.move_table):
-                  step = 0
-          if direction == self.LEFT:
-              if step < 0:
-                  step = len(self.move_table) - 1
-          time.sleep(self.delay)
+            step += direction
 
-  def cleanUp(self):
-      GPIO.cleanup()
+            if direction == Motor.RIGHT:
+                if step == len(self.move_table):
+                    step = 0
+            if direction == Motor.LEFT:
+                if step < 0:
+                    step = len(self.move_table) - 1
+            time.sleep(self.delay)
+
+
+    def cleanUp(self):
+        GPIO.cleanup()
