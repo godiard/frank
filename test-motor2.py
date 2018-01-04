36fbc547fcd5f051d2bc07ee55363b840c1687b4
Gonzalo Odiard <godiard@gmail.com>
2018-01-04 01:55:29 +0000

    Agrego test para el motor del otro eje

---
+++ test-motor2.py
@@ -0,0 +1,22 @@
+import RPi.GPIO as GPIO
+import time
+
+print "Set mode BCM"
+GPIO.setmode(GPIO.BCM)
+
+motors = [2, 3, 4, 17]
+
+for pin in motors:
+    print "Setup output port", pin
+    GPIO.setup(pin, GPIO.OUT)
+
+
+for i in range(500):
+    for pin in motors:
+        print "Switch", pin
+        GPIO.output(pin, 1)
+        time.sleep(0.01)
+        GPIO.output(pin, 0)
+
+
+GPIO.cleanup()
